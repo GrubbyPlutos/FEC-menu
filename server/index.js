3b43cf45efd87160c73f4d5a4e7747b16d991430
Dickson Kwok <dkwok1124@gmail.com>
2019-01-21 19:34:02 -0800

    Minor adjustment to server get request url
      - maintain consistency with client get request url

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+require('newrelic');
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
@@ -11,25 +12,23 @@ app.use(express.static('public'));
 app.get('/restaurants/:id', (req, res) => res.sendFile(path.resolve(__dirname, '..', 'public', 'index.html')));
 
 // Get all menu items of a certain restaurant
-app.get('/restaurants/:id/menu-items', async (req, res) => {
+app.get('/restaurants/:id/menu_items', async (req, res) => {
   const restaurantId = req.params.id;
   try {
     const fullMenu = await db.getFullMenu([restaurantId]);
     console.log('Retrieved from database:');
     res.status(200).send(fullMenu);
   } catch (err) {
-    console.error(err);
     res.sendStatus(500);
   }
 });
 
 // Get all categories and choices of a specific menu item in a restaurant
-app.get('/restaurants/:id/menu-items/:itemId', async (req, res) => {
+app.get('/restaurants/:id/menu_items/:itemId', async (req, res) => {
   try {
     const menuItem = await db.getMenuItem([req.params.id, req.params.itemId]);
-    res.send(menuItem);
+    res.status(200).send(menuItem);
   } catch (err) {
-    console.error(err);
     res.sendStatus(500);
   }
 });
